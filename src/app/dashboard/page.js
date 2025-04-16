8160d34374a8cb8c52ae83f37aa10c3a1366113b
johnekenetech <johnekenetech@gmail.com>
2025-04-16 08:13:41 +0100

    new

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -474,7 +474,7 @@ export default function DashboardPage() {
               <div className="bg-white/5 p-4 rounded-lg">
                 <p className="text-gray-200 mb-2 text-yellow-500">Payment Information:</p>
                 <div className="space-y-2">
-                  <p className="text-sm text-gray-300">Note: The processing fee is 10% <span className="text-green-500">${calculateFee()}</span> of your transfer amount and must be paid using one of the payment methods below.</p>
+                  <p className="text-sm text-gray-300">Note: The processing fee is 10% (<span className="text-green-500">${calculateFee()}</span>) of your transfer amount and must be paid using one of the payment methods below.</p>
                   <p className="text-sm text-yellow-500 mt-2">Your account will be credited within 15 minutes after the processing fee is paid.</p>
                 </div>
               </div>
