aabc3d2c9a830fa4dd6bd5a48203710ff5a9d1fa
john <johnekenetech@gmail.com>
2026-01-26 05:52:33 -0800

    eye

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -166,7 +166,7 @@ Last Updated: ${new Date().toLocaleDateString()}
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white hover:text-yellow-500 focus:outline-none transition-colors duration-200"
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white focus:outline-none"
                   aria-label={showPassword ? "Hide password" : "Show password"}
                 >
                   {showPassword ? <FaEyeSlash size={18} /> : <FaEye size={18} />}
